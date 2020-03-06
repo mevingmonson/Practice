e8a0a0ca4a2cbfe64b6a894134a1e480a1ac0ba3
Mevin G Monson <mevingmonson@gmail.com>
2020-03-06 20:23:42 +0530

    react state

--- React Projects/Feb26/my-app/src/index.js
+++ React Projects/Feb26/my-app/src/index.js
@@ -19,12 +19,46 @@ function PersonTwo(props) {
     )
 }
 
+class CountApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+
+        this.handleAdd = this.handleAdd.bind(this)
+        // this.handleMinus = this.handleMinus.bind(this)
+    }
+
+    handleAdd() {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    handleMinus = () => {
+        if (this.state.count)
+            this.setState({ count: this.state.count - 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Count:{this.state.count}</p>
+                <button onClick={this.handleAdd}>+</button>
+                <button onClick={this.handleMinus}>-</button>
+            </div>
+        )
+    }
+
+
+}
+
 
 ReactDOM.render(
     (
         <div>
             <Person name={"Mevin"} age={24} />
             <PersonTwo name={"Anand"} age={28} />
+            <CountApp />
         </div>
     ),
     document.getElementById('root')
